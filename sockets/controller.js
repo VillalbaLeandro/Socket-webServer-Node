1d2657c69beb0757b3205cc768145665ba82274d
VillalbaLeandro <exemprary@gmail.com>
2024-04-30 08:46:55 -0300

    se corriigio un error de tipeo en el nombre del evento

--- sockets/controller.js
+++ sockets/controller.js
@@ -7,7 +7,7 @@ const  socketController = (socket) => {
     socket.on('enviar-mensaje', (payload, callback) => {
         const id = 123456789;
         callback(id)
-        socket.broadcast.emit('envi ar-mensaje', payload)
+        socket.broadcast.emit('enviar-mensaje', payload)
     })
 }
 
