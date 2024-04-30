314788492bc81f957e9aaf8cac8a49487ad1cd29
VillalbaLeandro <exemprary@gmail.com>
2024-04-30 08:23:20 -0300

    Version 1.0.0 para deploy RC

--- sockets/controller.js
+++ sockets/controller.js
@@ -7,8 +7,6 @@ const  socketController = (socket) => {
     socket.on('enviar-mensaje', (payload, callback) => {
         const id = 123456789;
         callback(id)
-
-
         socket.broadcast.emit('envi ar-mensaje', payload)
     })
 }
